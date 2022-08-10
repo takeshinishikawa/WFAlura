6e2afaeba8e83735465cc2feeccd18545b4a64d8
takeshinishikawa <takeshinishikawa@gmail.com>
2022-08-10 11:19:22 -0300

    added CPF validation exercise + mask

--- CursoWindowsForms/Program.cs
+++ CursoWindowsForms/Program.cs
@@ -11,7 +11,7 @@ namespace CursoWindowsForms
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Frm_Mascara());
+            Application.Run(new Frm_ValidaCPF());
         }
     }
 }
